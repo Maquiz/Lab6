bf283ec643d93e2f494e7c13cbff51e00920c0fa
Thomas A <curioustommy@users.noreply.github.com>
2019-03-01 14:23:30 -0800

    Number are fun!

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int evil_number = 667;
 		System.out.println("Hello World");
+		evil_number--;
 		//Hello World
 	}
 
